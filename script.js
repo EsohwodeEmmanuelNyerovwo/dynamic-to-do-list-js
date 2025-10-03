5c1e32856b83150df0716c8f07690057802fd183
EsohwodeEmmanuelNyerovwo <emmasonnyerovwo@gmail.com>
2025-10-03 14:32:13 -0700

    dynamic todo list

--- script.js
+++ script.js
@@ -7,25 +7,27 @@ document.addEventListener('DOMContentLoaded', function () {
     //function to add task
     function addTask() {
         //check if textbox is empty and also perform the trim function
-        const taskText = (taskInput.value);
-        if (taskText === ''.trim()) {
+        const taskText = (taskInput.value).trim();
+        if (taskText === '') {
             alert('Enter a task');
             return;
         }
-        //create the li element
-        const li = document.createElement('li');
-        li.textContent = taskText;
-        const removeBtn = document.createElement('button');
-        removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
-        removeBtn.onclick = function () {
-            taskList.removeChild(li);
-        }
-        li.appendChild(removeBtn);
-        taskList.appendChild(li);
+        else {
+            //create the li element
+            const li = document.createElement('li');
+            li.textContent = taskText;
+            const removeBtn = document.createElement('button');
+            removeBtn.textContent = 'Remove';
+            removeBtn.className = 'remove-btn';
+            removeBtn.onclick = function () {
+                taskList.removeChild(li);
+            }
+            li.appendChild(removeBtn);
+            taskList.appendChild(li);
 
-        //clear the input
-        taskInput.value = '';
+            //clear the input
+            taskInput.value = '';
+        }
     }
     //Attach EventListeners
     addButton.addEventListener('click', (event) => {
